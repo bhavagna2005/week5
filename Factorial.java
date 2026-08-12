2a92b80b0b0553d0837808721d352a6db203d589
bhavagna2005 <23251a1255@gmail.com>
2026-08-12 10:17:50 +0530

    Refactor factorial method to instance method

--- Factorial.java
+++ Factorial.java
@@ -1,20 +1,12 @@
 public class Factorial {
 
-    public static long factorial(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Number cannot be negative");
-        }
-
-        long result = 1;
+    public long calculate(int n) {
+        long fact = 1;
 
         for (int i = 1; i <= n; i++) {
-            result *= i;
+            fact = fact * i;
         }
 
-        return result;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Factorial of 5 = " + factorial(5));
+        return fact;
     }
 }
